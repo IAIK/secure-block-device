ca9e206f177993f741fc1bb7d1745f9f50c9be5c
Daniel Hein <daniel.hein@iaik.tugraz.at>
2014-05-14 15:09:31 +0200

    fix missing macros in siv implementation

--- src/crypto/siv.h
+++ src/crypto/siv.h
@@ -56,6 +56,23 @@ typedef struct _siv_ctx {
     AES_KEY s2v_sched;
 } siv_ctx;
 
+#ifdef AES_LONG
+typedef unsigned long u32;
+#else
+typedef unsigned int u32;
+#endif
+typedef unsigned short u16;
+typedef unsigned char u8;
+
+#if defined(_MSC_VER) && (defined(_M_IX86) || defined(_M_AMD64) || defined(_M_X64))
+# define SWAP(x) (_lrotl(x, 8) & 0x00ff00ff | _lrotr(x, 8) & 0xff00ff00)
+# define GETU32(p) SWAP(*((u32 *)(p)))
+# define PUTU32(ct, st) { *((u32 *)(ct)) = SWAP((st)); }
+#else
+# define GETU32(pt) (((u32)(pt)[0] << 24) ^ ((u32)(pt)[1] << 16) ^ ((u32)(pt)[2] << 8) ^ ((u32)(pt)[3]))
+# define PUTU32(ct, st) { (ct)[0] = (u8)((st) >> 24); (ct)[1] = (u8)((st) >> 16); (ct)[2] = (u8)((st) >> 8); (ct)[3] = (u8)(st); }
+#endif
+
 /*
  * exported APIs
  */
