5ef12f971fdf53c0b3d41388e137915511e38a7a
Daniel Hein <daniel.hein@iaik.tugraz.at>
2014-05-18 16:54:54 +0200

    start write block with caching

--- src/sbdi_block_layer.c
+++ src/sbdi_block_layer.c
@@ -16,6 +16,8 @@
 #include <string.h>
 #include <stdarg.h>
 
+#define SBDI_CACHE_SIZE ((SBDI_CACHE_MAX_SIZE)  * (SBDI_BLOCK_SIZE))
+
 typedef struct secure_block_device_interface {
   int fd;
   siv_ctx *ctx;
@@ -29,8 +31,9 @@ typedef uint8_t sbdi_tag_t[SBDI_BLOCK_TAG_SIZE];
 sbdi_error_t sbdi_bl_read_block(const sbdi_t *sbdi, sbdi_block_t *blk,
     size_t len)
 {
-  if (!sbdi || !blk || blk->idx > SBDI_BLOCK_MAX_INDEX
-      || len == 0|| len > SBDI_BLOCK_SIZE) {
+  if (!sbdi || !blk || !blk->data || blk->idx > SBDI_BLOCK_MAX_INDEX
+      || len == 0|| len > SBDI_BLOCK_SIZE || blk->data < sbdi->cache->store
+      || blk->data > sbdi->cache->store + SBDI_CACHE_SIZE) {
     return SBDI_ERR_ILLEGAL_PARAM;
   }
   ssize_t r = pread(sbdi->fd, blk->data, len, blk->idx * SBDI_BLOCK_SIZE);
@@ -59,40 +62,65 @@ sbdi_error_t sbdi_bl_cache_decrypt(sbdi_t *sbdi, sbdi_block_t *blk, size_t len,
   }
   r = sbdi_bl_read_block(sbdi, blk, len);
   if (r != SBDI_SUCCESS) {
-    // TODO free block reservation in cache!
+    sbdi_bc_evict_blk(sbdi->cache, blk);
     return r;
   }
   siv_restart(sbdi->ctx);
   int cr = -1;
   if (blk_ctr == NULL) {
+    // Add block index as additional information to the decryption
     cr = siv_decrypt(sbdi->ctx, blk->data[0], blk->data[0], SBDI_BLOCK_SIZE,
         *tag, 1, &blk->idx, sizeof(uint32_t));
   } else {
+    // Add block index and block counter as additional information to the decryption
     cr = siv_decrypt(sbdi->ctx, blk->data[0], blk->data[0], SBDI_BLOCK_SIZE,
         *tag, 2, &blk->idx, sizeof(uint32_t), blk_ctr, ctr_len);
   }
   if (cr == -1) {
-    // TODO free block reservation in cache!
+    sbdi_bc_evict_blk(sbdi->cache, blk);
     return SBDI_ERR_CRYPTO_FAIL;
   }
   return SBDI_SUCCESS;
 }
 
 //----------------------------------------------------------------------
-sbdi_error_t sbdi_fb_read_data_block(sbdi_t *sbdi, unsigned int *ptr,
+static inline uint32_t sbdi_get_mngt_block_number(uint32_t idx)
+{
+  return idx / SBDI_MNGT_BLOCK_ENTRIES;
+}
+
+//----------------------------------------------------------------------
+static inline uint32_t sbdi_get_mngt_block_index(uint32_t idx)
+{
+  return sbdi_get_mngt_block_number() * SBDI_MNGT_BLOCK_ENTRIES;
+}
+
+//----------------------------------------------------------------------
+static inline uint32_t sbdi_get_data_block_index(uint32_t idx)
+{
+  return idx + sbdi_get_mngt_block_number() + 2;
+}
+
+//----------------------------------------------------------------------
+static inline uint32_t sbdi_get_mngt_tag_index(uint32_t idx)
+{
+  return idx % SBDI_MNGT_BLOCK_ENTRIES;
+}
+
+//----------------------------------------------------------------------
+sbdi_error_t sbdi_bl_read_data_block(sbdi_t *sbdi, unsigned char *ptr,
     uint32_t idx, size_t len)
 {
   if (!sbdi || !ptr || idx > SBDI_BLOCK_MAX_INDEX
       || len == 0|| len > SBDI_BLOCK_SIZE) {
-    // TODO Question Johannes about 'deep' error checking (internal struct state)
     return SBDI_ERR_ILLEGAL_PARAM;
   }
   // TODO check that read is not beyond the bounds of file, otherwise the
   // Merkle tree won't work correctly!
-  uint32_t mng_nbr = idx / SBDI_MNGT_BLOCK_ENTRIES;
-  uint32_t mng_idx = mng_nbr * SBDI_MNGT_BLOCK_ENTRIES;
-  uint32_t dat_idx = idx + mng_nbr + 2;
-  uint32_t tag_idx = idx % SBDI_MNGT_BLOCK_ENTRIES;
+  uint32_t mng_nbr = sbdi_get_mngt_block_number(idx);
+  uint32_t mng_idx = sbdi_get_mngt_block_index(idx);
+  uint32_t dat_idx = sbdi_get_data_block_index(idx);
+  uint32_t tag_idx = sbdi_get_mngt_tag_index(idx);
 
   sbdi_error_t r;
   sbdi_block_t mng_dat, blk_dat;
@@ -105,30 +133,13 @@ sbdi_error_t sbdi_fb_read_data_block(sbdi_t *sbdi, unsigned int *ptr,
     return r;
   }
   if (!mngt->data) {
-//    // Block not yet in cache
+    // Block not yet in cache
     sbdi_tag_t mng_tag;
     r = sbdi_bl_cache_decrypt(sbdi, mngt, len, &mng_tag, NULL, 0);
     if (r != SBDI_SUCCESS) {
       // TODO Cleanup?
       return r;
     }
-//    r = sbdi_bc_cache_blk(sbdi->cache, mng_idx, &mngt);
-//    if (r != SBDI_SUCCESS) {
-//      return r;
-//    } else if (!mngt) {
-//      return SBDI_ERR_ILLEGAL_STATE;
-//    }
-//    r = sbdi_bl_read_block(sbdi, *mngt, mng_idx, SBDI_BLOCK_SIZE);
-//    if (r != SBDI_SUCCESS) {
-//      // TODO free block reservation in cache!
-//      return r;
-//    }
-//    // Add block index as additional information to the decryption
-//    if (!siv_decrypt(sbdi->ctx, *mngt, *mngt, SBDI_BLOCK_SIZE, mng_tag, 1,
-//        &mng_idx, sizeof(uint32_t))) {
-//      // TODO free block reservation in cache!
-//      return SBDI_ERR_CRYPTO_FAIL;
-//    }
     // TODO add management tag to Merkle tree and validate root
     mt_error_t mt_r;
     mt_r = mt_verify(sbdi->mt, NULL, mng_nbr);
@@ -145,7 +156,7 @@ sbdi_error_t sbdi_fb_read_data_block(sbdi_t *sbdi, unsigned int *ptr,
   if (!(blk->data)) {
     sbdi_tag_t tag;
     r = sbdi_bl_cache_decrypt(sbdi, blk, SBDI_BLOCK_SIZE, &tag, blk_ctr,
-        SBDI_BLOCK_ACCESS_COUNTER_SIZE);
+    SBDI_BLOCK_ACCESS_COUNTER_SIZE);
     if (r != SBDI_SUCCESS) {
       // TODO Cleanup?
       return r;
@@ -159,3 +170,45 @@ sbdi_error_t sbdi_fb_read_data_block(sbdi_t *sbdi, unsigned int *ptr,
   return SBDI_SUCCESS;
 }
 
+//----------------------------------------------------------------------
+sbdi_error_t sbdi_bl_write_block(const sbdi_t *sbdi, sbdi_block_t *blk,
+    size_t len)
+{
+  if (!sbdi || !blk || !blk->data || blk->idx > SBDI_BLOCK_MAX_INDEX
+      || len == 0|| len > SBDI_BLOCK_SIZE || blk->data < sbdi->cache->store
+      || blk->data > sbdi->cache->store + SBDI_CACHE_SIZE) {
+    return SBDI_ERR_ILLEGAL_PARAM;
+  }
+  ssize_t r = pwrite(sbdi->fd, blk->data, len, blk->idx * SBDI_BLOCK_SIZE);
+  if (r == -1) {
+    return SBDI_ERR_IO;
+  } else if (r < len) {
+    return SBDI_ERR_MISSING_DATA;
+  }
+  return SBDI_SUCCESS;
+}
+
+//----------------------------------------------------------------------
+sbdi_error_t sbdi_bl_write_data_block(sbdi_t *sbdi, unsigned char *ptr,
+    uint32_t idx, size_t len)
+{
+  if (!sbdi || !ptr || idx > SBDI_BLOCK_MAX_INDEX
+      || len == 0|| len > SBDI_BLOCK_SIZE) {
+    return SBDI_ERR_ILLEGAL_PARAM;
+  }
+  // TODO check that read is not beyond the bounds of file, otherwise the
+  // Merkle tree won't work correctly!
+  uint32_t mng_nbr = sbdi_get_mngt_block_number(idx);
+  uint32_t mng_idx = sbdi_get_mngt_block_index(idx);
+  uint32_t dat_idx = sbdi_get_data_block_index(idx);
+  uint32_t tag_idx = sbdi_get_mngt_tag_index(idx);
+  sbdi_error_t r;
+  sbdi_block_t mng_dat, blk_dat;
+  sbdi_block_t *mngt = &mng_dat;
+  sbdi_block_t *blk = &blk_dat;
+  sbdi_block_init(mngt, mng_idx, NULL);
+  sbdi_block_init(blk, dat_idx, NULL);
+  // Make sure block is in cache
+  // TODO kind of call read block without the copying part and also get block counter from management block
+  //memcpy(blk->data, ptr, len);
+}
