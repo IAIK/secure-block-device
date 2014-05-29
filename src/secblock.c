2a1764da41f1af2d5ebd72a766af4c7354e3500a
Daniel Hein <daniel.hein@iaik.tugraz.at>
2014-05-29 16:30:28 +0200

    add function to read header and general code cleanup

--- src/secblock.c
+++ src/secblock.c
@@ -5,52 +5,71 @@
  *      Author: dhein
  */
 
+#include "siv.h"
+
 #include "secblock.h"
+#include "sbdi_block.h"
+#include "sbdi_buffer.h"
 
 #include <string.h>
-#include <stdio.h>
 
 //----------------------------------------------------------------------
-sbdi_error_t sbdi_create_hdr(sbdi_hdr_t **hdr)
+sbdi_error_t sbdi_create_hdr_v1(sbdi_hdr_v1_t **hdr)
 {
-  if (!hdr) {
-    return SBDI_ERR_ILLEGAL_PARAM;
-  }
-  sbdi_hdr_t *lhdr = calloc(1, sizeof(sbdi_hdr_t));
-  if (!lhdr) {
+  SBDI_CHK_PARAM(hdr);
+  sbdi_hdr_v1_t *h = calloc(1, sizeof(sbdi_hdr_v1_t));
+  if (!h) {
     return SBDI_ERR_OUT_Of_MEMORY;
   }
-  lhdr->version = SBDI_HDR_VERSION_1;
-  *hdr = lhdr;
+  memcpy(h->id.magic, SBDI_HDR_MAGIC, SBDI_HDR_MAGIC_LEN);
+  h->id.version = SBDI_HDR_VERSION_1;
+  sbdi_ctr_128b_init(&h->ctr, 0, 0);
+  // TODO set key, set tag
+  *hdr = h;
   return SBDI_SUCCESS;
 }
 
-//----------------------------------------------------------------------
-static sbdi_error_t sbdi_check_hdr(sbdi_hdr_t *hdr)
+void sbdi_delete_hdr_v1(sbdi_hdr_v1_t *hdr)
 {
-  if (hdr == NULL) {
-    return SBDI_ERR_ILLEGAL_PARAM;
+  // This should remove traces of the key from memory
+  // TODO do the same for the SBDI data structure
+  memset(hdr, 0, sizeof(sbdi_hdr_v1_t));
+  free(hdr);
+}
+
+//----------------------------------------------------------------------
+sbdi_error_t sbdi_read_hdr_v1(sbdi_t *sbdi, sbdi_hdr_v1_t **hdr)
+{
+  SBDI_CHK_PARAM(sbdi && hdr);
+  siv_ctx ctx;
+
+  sbdi_hdr_v1_t *h = calloc(1, sizeof(sbdi_hdr_v1_t));
+  if (!h) {
+    return SBDI_ERR_OUT_Of_MEMORY;
   }
-  if (hdr->version != 1) {
+  sbdi_bl_read_hdr_block(sbdi, *sbdi->write_store[0].data,
+      SBDI_HDR_V1_PACKED_SIZE);
+  sbdi_buffer_t b;
+  sbdi_buffer_init(&b, *sbdi->write_store[0].data, SBDI_HDR_V1_PACKED_SIZE);
+  for (int i = 0; i < SBDI_HDR_MAGIC_LEN; ++i) {
+    h->id.magic[i] = sbdi_buffer_read_uint8_t(&b);
+  }
+  if (!memcmp(h->id.magic, SBDI_HDR_MAGIC, SBDI_HDR_MAGIC_LEN)) {
+    return SBDI_ERR_ILLEGAL_STATE;
+  }
+  h->id.version = sbdi_buffer_read_uint32_t(&b);
+  if (h->id.version > SBDI_HDR_SUPPORTED_VERSION) {
     return SBDI_ERR_UNSUPPORTED;
   }
-  printf("Nbr. of key slots: %lx", (unsigned long) sizeof(hdr->key_slots));
-  if (hdr->key_slots[0].key_alg_id != SBDI_HDR_KS_ALG_AES) {
-    return SBDI_ERR_UNSUPPORTED;
-  }
-  if (hdr->key_slots[0].key_size != SBDI_HDR_V1_KS0_KEY_SIZE) {
-    return SBDI_ERR_UNSUPPORTED;
-  }
-  for (int i = 1; i < sizeof(hdr->key_slots); ++i) {
-//    // Make sure only supported algorithms are present
-//    // This check might be softened up to if not used ==> don't care
-//    if (hdr->key_slots[i].key_alg_id != SBDI_HDR_KS_ALG_AES ||
-//        hdr->key_slots[i].key_alg_id != SBDI_HDR_KS_ALG_RSA) {
-//      return SBDI_ERR_UNSUPPORTED;
-//    }
-    if (hdr->key_slots[i].key_size > SBDI_HDR_V1_KS_MAX_KEY_SIZE) {
-      return SBDI_ERR_UNSUPPORTED;
-    }
+  // TODO write buffer extensions to read global counter and keys
+  // TODO make sure all global counter values are packed in the same way!
+  sbdi_ctr_128b_init(&h->ctr, sbdi_buffer_read_uint64_t(&b), sbdi_buffer_read_uint64_t(&b));
+  sbdi_buffer_read_bytes(&b, h->tag, sizeof(sbdi_tag_t));
+  sbdi_buffer_read_bytes(&b, h->key, SBDI_HDR_V1_KEY_SIZE);
+  siv_init(&ctx, sbdi_siv_master_key, SIV_256);
+  int cr = siv_decrypt(&ctx, h->key, h->key, SBDI_HDR_V1_KEY_SIZE, h->tag, 0);
+  if (cr == -1) {
+    return SBDI_ERR_TAG_MISMATCH;
   }
   return SBDI_SUCCESS;
 }
@@ -58,23 +77,5 @@ static sbdi_error_t sbdi_check_hdr(sbdi_hdr_t *hdr)
 //----------------------------------------------------------------------
 int sbdi_open(void)
 {
-//  int fd;
-  sbdi_error_t err;
-  sbdi_hdr_t *hdr = NULL;
-//  err = sdbi_fb_open(&fd);
-//  if (err != SBDI_SUCCESS) {
-//    // TODO Set error flag?
-//    return -1;
-//  }
-//  err = sbdi_fb_read_hdr(fd, &hdr);
-//  if (err != SBDI_SUCCESS) {
-//    // TODO Set error flag?
-//    return -1;
-//  }
-  err = sbdi_check_hdr(hdr);
-  if (err != SBDI_SUCCESS) {
-    // TODO Set error flag?
-    return -1;
-  }
   return 0;
 }
--- src/secblock.h
+++ src/secblock.h
@@ -17,30 +17,23 @@ extern "C" {
 
 #include <sys/types.h>
 #include <stdint.h>
-#include <stdlib.h>
 
-#define SBDI_HDR_VERSION_1 1
-#define SBDI_HDR_MAGIC_LEN 8
+#define SBDI_HDR_VERSION_1 1u
+#define SBDI_HDR_MAGIC_LEN 8u
 
 #define SBDI_HDR_SUPPORTED_VERSION SBDI_HDR_VERSION_1
 
-#define SBDI_HDR_KS_ALG_AES 0x00000001
-#define SBDI_HDR_KS_ALG_RSA 0x80000001
+#define SBDI_HDR_V1_PACKED_SIZE 60u
+#define SBDI_HDR_V1_KEY_SIZE    32u
 
-#define SBDI_HDR_V1_TAG_LEN 16
-#define SBDI_HDR_V1_KS0_ADDR 0x100
-#define SBDI_HDR_V1_KS0_KEY_SIZE 32
-#define SBDI_HDR_V1_KS_MAX_KEY_SIZE 512
+typedef uint8_t sbdi_hdr_magic_t[SBDI_HDR_MAGIC_LEN];
+typedef uint8_t sbdi_hdr_v1_sym_key_t[SBDI_HDR_V1_KEY_SIZE];
 
-typedef uint8_t sbdi_magic_t[SBDI_HDR_MAGIC_LEN];
-typedef uint8_t sbdi_sym_key_t[SBDI_HDR_V1_KS0_KEY_SIZE];
-typedef uint8_t sbdi_tag_t[SBDI_HDR_V1_TAG_LEN];
-
-static const sbdi_magic_t SBDI_MAGIC = {
+static const sbdi_hdr_magic_t SBDI_HDR_MAGIC = {
     0xA1, 0x1D, 0x1F, 0xDE, 0xAD, 0xDA, 0x7A, 0xFF
 };
 
-static const uint8_t sbdi_siv_master_key[32] = {
+static const uint8_t sbdi_siv_master_key[SBDI_HDR_V1_KEY_SIZE] = {
     0xa7, 0xde, 0x2e, 0xb8, 0xf7, 0xc2, 0x85, 0xa6,
     0x66, 0x27, 0x9c, 0xa4, 0x8e, 0x4c, 0xb5, 0xda,
     0x98, 0xaf, 0x8c, 0x50, 0x5d, 0xe6, 0x4a, 0xf0,
@@ -48,24 +41,23 @@ static const uint8_t sbdi_siv_master_key[32] = {
 };
 
 /*!
- * TODO Description
+ * \brief holds the information necessary to identify a secure block device
+ * interface header and its version
  */
-typedef struct secure_block_device_interface_key_slot {
-	uint8_t  key_uuid[16]; /*!< the universal unique id based key identifier */
-	uint32_t key_alg_id;   /*!< an identifier for the algorithm used to encrypt the block device key */
-	uint32_t key_size;     /*!< the length of the encrypted blob of secure block device key blob */
-} sbdi_ks_t;
+typedef struct secure_block_device_interface_header_id {
+  sbdi_hdr_magic_t magic; //!< the magic number identifying a secure block device interface
+  uint32_t version; //!< the version of the secure block device interface header
+} sbdi_hdr_id_t;
 
 /*!
- * TODO Description
+ * \brief the secure block device interface header
  */
-typedef struct secure_block_device_interface_header {
-  sbdi_magic_t magic;
-	uint32_t version;
-	sbdi_ctr_128b_t ctr;
-	uint8_t  tag[SBDI_HDR_V1_TAG_LEN];
-	sbdi_ks_t key_slots[8];
-} sbdi_hdr_t;
+typedef struct secure_block_device_interface_header_v1 {
+  sbdi_hdr_id_t id; //!< the secure block device interface identification information
+	sbdi_ctr_128b_t ctr; //!< access counter protecting the header against replay attacks
+	sbdi_tag_t tag; //!< the tag protecting the integrity of the key
+	sbdi_hdr_v1_sym_key_t key; //!< the encrypted secure block device key
+} sbdi_hdr_v1_t;
 
 int sbdi_open(void);
 ssize_t sbdi_pread(int fildes, void *buf, size_t nbyte, off_t offset);
