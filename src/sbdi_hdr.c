80c3f382a2fdcd12c7f96b86db72dfdd7790819f
Daniel Hein <daniel.hein@iaik.tugraz.at>
2014-05-30 11:08:27 +0200

    move all sbdi header related code into own compilation unit

---
+++ src/sbdi_hdr.c
@@ -0,0 +1,125 @@
+/*
+ * sbdi_hdr.c
+ *
+ *  Created on: May 30, 2014
+ *      Author: dhein
+ */
+
+#include "sbdi_hdr.h"
+#include "sbdi_buffer.h"
+
+#include <string.h>
+
+//----------------------------------------------------------------------
+void sbdi_derive_hdr_v1_key(siv_ctx *master, sbdi_hdr_v1_sym_key_t key,
+    uint8_t *n1, size_t n1_len, uint8_t *n2, size_t n2_len)
+{
+  memset(key, 0, sizeof(sbdi_hdr_v1_sym_key_t));
+  vprf(master, key, 1, n1, n1_len);
+  vprf(master, key + SBDI_BLOCK_TAG_SIZE, 1, n2, n2_len);
+}
+
+//----------------------------------------------------------------------
+sbdi_error_t sbdi_create_hdr_v1(sbdi_hdr_v1_t **hdr,
+    const sbdi_hdr_v1_sym_key_t key)
+{
+  SBDI_CHK_PARAM(hdr && key);
+  sbdi_hdr_v1_t *h = calloc(1, sizeof(sbdi_hdr_v1_t));
+  if (!h) {
+    return SBDI_ERR_OUT_Of_MEMORY;
+  }
+  memcpy(h->id.magic, SBDI_HDR_MAGIC, SBDI_HDR_MAGIC_LEN);
+  h->id.version = SBDI_HDR_VERSION_1;
+  sbdi_error_t r = sbdi_ctr_128b_init(&h->ctr, 0, 0);
+  if (r != SBDI_SUCCESS) {
+    free(h);
+    return r;
+  }
+  // Tag will be created once the header is written
+  memset(h->tag, 0, sizeof(sbdi_tag_t));
+  // Copy previously created key into header
+  memcpy(h->key, key, sizeof(sbdi_hdr_v1_sym_key_t));
+  *hdr = h;
+  return SBDI_SUCCESS;
+}
+
+void sbdi_delete_hdr_v1(sbdi_hdr_v1_t *hdr)
+{
+// This should remove traces of the key from memory
+// TODO do the same for the SBDI data structure
+  assert(hdr);
+  memset(hdr, 0, sizeof(sbdi_hdr_v1_t));
+  free(hdr);
+}
+
+//----------------------------------------------------------------------
+sbdi_error_t sbdi_read_hdr_v1(sbdi_t *sbdi, sbdi_hdr_v1_t **hdr,
+    siv_ctx *master)
+{
+  SBDI_CHK_PARAM(sbdi && hdr && master);
+  sbdi_hdr_v1_t *h = calloc(1, sizeof(sbdi_hdr_v1_t));
+  if (!h) {
+    return SBDI_ERR_OUT_Of_MEMORY;
+  }
+  uint8_t *rd_buf = *sbdi->write_store[0].data;
+  sbdi_error_t r = sbdi_bl_read_hdr_block(sbdi, rd_buf,
+      SBDI_HDR_V1_PACKED_SIZE);
+  if (r != SBDI_SUCCESS) {
+    free(h);
+    return r;
+  }
+  sbdi_buffer_t b;
+  sbdi_buffer_init(&b, rd_buf, SBDI_HDR_V1_PACKED_SIZE);
+  sbdi_buffer_read_bytes(&b, h->id.magic, SBDI_HDR_MAGIC_LEN);
+  if (!memcmp(h->id.magic, SBDI_HDR_MAGIC, SBDI_HDR_MAGIC_LEN)) {
+    free(h);
+    return SBDI_ERR_ILLEGAL_STATE;
+  }
+  h->id.version = sbdi_buffer_read_uint32_t(&b);
+  if (h->id.version > SBDI_HDR_SUPPORTED_VERSION) {
+    free(h);
+    return SBDI_ERR_UNSUPPORTED;
+  }
+// TODO make sure all global counter values are packed in the same way!
+  r = sbdi_buffer_read_ctr_128b(&b, &h->ctr);
+  if (r != SBDI_SUCCESS) {
+    free(h);
+    return r;
+  }
+  uint8_t *kptr = sbdi_buffer_get_cptr(&b);
+  sbdi_buffer_add_pos(&b, SBDI_HDR_V1_KEY_SIZE);
+  sbdi_buffer_read_bytes(&b, h->tag, SBDI_HDR_V1_TAG_SIZE);
+  int cr = siv_decrypt(master, kptr, h->key, SBDI_HDR_V1_KEY_SIZE, h->tag, 0);
+  if (cr == -1) {
+    free(h);
+    return SBDI_ERR_TAG_MISMATCH;
+  }
+  r = sbdi_bl_verify_header(sbdi, rd_buf, SBDI_HDR_V1_PACKED_SIZE);
+  if (r != SBDI_SUCCESS) {
+    free(h);
+    return r;
+  }
+  *hdr = h;
+  return SBDI_SUCCESS;
+}
+
+//----------------------------------------------------------------------
+sbdi_error_t sbdi_write_hdr_v1(sbdi_t *sbdi, const sbdi_hdr_v1_t *hdr,
+    siv_ctx *master)
+{
+  SBDI_CHK_PARAM(sbdi && hdr);
+  uint8_t *wrt_buf = *sbdi->write_store[0].data;
+  sbdi_buffer_t b;
+  sbdi_buffer_init(&b, wrt_buf, SBDI_HDR_V1_PACKED_SIZE);
+  sbdi_buffer_write_bytes(&b, hdr->id.magic, SBDI_HDR_MAGIC_LEN);
+  sbdi_buffer_write_uint32_t(&b, hdr->id.version);
+  sbdi_buffer_write_ctr_128b(&b, &hdr->ctr);
+  uint8_t *kptr = sbdi_buffer_get_cptr(&b);
+  sbdi_buffer_add_pos(&b, SBDI_HDR_V1_KEY_SIZE);
+  uint8_t *tptr = sbdi_buffer_get_cptr(&b);
+  sbdi_buffer_add_pos(&b, SBDI_HDR_V1_TAG_SIZE);
+  siv_encrypt(master, hdr->key, kptr, SBDI_HDR_V1_KEY_SIZE, tptr, 0);
+  sbdi_bl_write_hdr_block(sbdi, wrt_buf, SBDI_HDR_V1_PACKED_SIZE);
+  return SBDI_SUCCESS;
+}
+
--- src/secblock.h
+++ src/secblock.h
@@ -12,58 +12,23 @@ extern "C" {
 #ifndef SECBLOCK_H_
 #define SECBLOCK_H_
 
-#include "sbdi_config.h"
-#include "sbdi_ctr_128b.h"
+#include "sbdi_block.h"
 
 #include <sys/types.h>
 #include <stdint.h>
 
-#define SBDI_HDR_VERSION_1 1u
-#define SBDI_HDR_MAGIC_LEN 8u
-
-#define SBDI_HDR_SUPPORTED_VERSION SBDI_HDR_VERSION_1
-
-#define SBDI_HDR_V1_PACKED_SIZE 76u
-#define SBDI_HDR_V1_KEY_SIZE    32u
-#define SBDI_HDR_V1_TAG_SIZE    SBDI_BLOCK_TAG_SIZE
-
-typedef uint8_t sbdi_hdr_magic_t[SBDI_HDR_MAGIC_LEN];
-typedef uint8_t sbdi_hdr_v1_sym_key_t[SBDI_HDR_V1_KEY_SIZE];
-
-static const sbdi_hdr_magic_t SBDI_HDR_MAGIC = {
-    0xA1, 0x1D, 0x1F, 0xDE, 0xAD, 0xDA, 0x7A, 0xFF
-};
-
-static const uint8_t sbdi_siv_master_key[SBDI_HDR_V1_KEY_SIZE] = {
+static const uint8_t sbdi_siv_master_key[32] = {
     0xa7, 0xde, 0x2e, 0xb8, 0xf7, 0xc2, 0x85, 0xa6,
     0x66, 0x27, 0x9c, 0xa4, 0x8e, 0x4c, 0xb5, 0xda,
     0x98, 0xaf, 0x8c, 0x50, 0x5d, 0xe6, 0x4a, 0xf0,
     0x29, 0x87, 0x6e, 0x34, 0x4c, 0x0b, 0x9b, 0x5a
 };
 
-/*!
- * \brief holds the information necessary to identify a secure block device
- * interface header and its version
- */
-typedef struct secure_block_device_interface_header_id {
-  sbdi_hdr_magic_t magic; //!< the magic number identifying a secure block device interface
-  uint32_t version; //!< the version of the secure block device interface header
-} sbdi_hdr_id_t;
 
-/*!
- * \brief the secure block device interface header
- */
-typedef struct secure_block_device_interface_header_v1 {
-  sbdi_hdr_id_t id; //!< the secure block device interface identification information
-	sbdi_ctr_128b_t ctr; //!< access counter protecting the header against replay attacks
-	sbdi_tag_t tag; //!< the tag protecting the integrity of the key
-	sbdi_hdr_v1_sym_key_t key; //!< the encrypted secure block device key
-} sbdi_hdr_v1_t;
-
-int sbdi_open(void);
-ssize_t sbdi_pread(int fildes, void *buf, size_t nbyte, off_t offset);
-ssize_t sbdi_pwrite(int fildes, const void *buf, size_t nbyte, off_t offset);
-void sbdi_close(int filedes);
+sbdi_t *sbdi_open(void);
+ssize_t sbdi_pread(sbdi_t *sbdi, void *buf, size_t nbyte, off_t offset);
+ssize_t sbdi_pwrite(sbdi_t *sbdi, const void *buf, size_t nbyte, off_t offset);
+void sbdi_close(sbdi_t *sbdi);
 
 #endif /* SECBLOCK_H_ */
 
