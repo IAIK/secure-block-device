8e696c3dd69dacbc86ac218c9f105e13cbbd383e
Daniel Hein <daniel.hein@iaik.tugraz.at>
2014-05-30 09:10:38 +0200

    update buffer header

--- src/sbdi_buffer.h
+++ src/sbdi_buffer.h
@@ -15,12 +15,14 @@ extern "C" {
 #include <stdint.h>
 #include <stdlib.h>
 
+#include "sbdi_ctr_128b.h"
+
 /*!
  * \brief defines data type for reading and writing data to and from a byte
  * buffer
  */
 typedef struct sbdi_buffer {
-  unsigned char *buffer; //!< the actual byte data buffer
+  uint8_t *buffer; //!< the actual byte data buffer
   size_t length;         //!< the total length of the buffer in bytes
   size_t pos; //!< the current position where the next read or write operation will be performed
 } sbdi_buffer_t;
@@ -41,6 +43,27 @@ void sbdi_buffer_init(sbdi_buffer_t *buf, uint8_t *buffer, const size_t length);
  * @param buf[inout] a pointer to the buffer to reset
  */
 void sbdi_buffer_reset(sbdi_buffer_t *buf);
+
+/*!
+ * \brief Returns a pointer to the data buffer underlying the SBDI buffer
+ * that points to the current offset of the SBDI buffer
+ *
+ * Basically, this function provides unsafe access to the underlying data
+ * buffer. The caller must ensure that the position is updated, if necessary.
+ *
+ * @param buf[in] a pointer to the buffer from which to extract the pointer
+ * @return a pointer to the underlying data buffer at the current SBDI buffer
+ * offset
+ */
+uint8_t *sbdi_buffer_get_cptr(const sbdi_buffer_t *buf);
+
+/*!
+ * \brief Add a value to the position pointer (offset) of the SBDI buffer
+ *
+ * @param buf[inout] a pointer to the buffer to update
+ * @param add[in] the value to add to the position
+ */
+void sbdi_buffer_add_pos(sbdi_buffer_t *buf, const uint32_t add);
 
 /*!
  * \brief Writes the given unsigned 8 bit integer into the buffer
@@ -73,6 +96,24 @@ void sbdi_buffer_write_uint32_t(sbdi_buffer_t *buf, const uint32_t value);
  * @param value[in] the value to write
  */
 void sbdi_buffer_write_uint64_t(sbdi_buffer_t *buf, const uint64_t value);
+
+/*!
+ * \brief Writes length bytes from src into the buffer
+ *
+ * @param buf[inout] a pointer to the buffer to write to
+ * @param src[in] a pointer to the data buffer to read from
+ * @param length[in] the number of bytes to copy from src into the buffer
+ */
+void sbdi_buffer_write_bytes(sbdi_buffer_t *buf, const uint8_t *src,
+    const size_t length);
+
+/*!
+ * \brief Writes the given 128 bit counter into the buffer
+ *
+ * @param buf[inout] a pointer to the buffer to write to
+ * @param ctr[in] a pointer to the 128 bit counter to write into the buffer
+ */
+void sbdi_buffer_write_ctr_128b(sbdi_buffer_t *buf, const sbdi_ctr_128b_t *ctr);
 
 /*!
  * \brief Reads a unsigned 8 bit integer from the buffer
@@ -117,13 +158,14 @@ void sbdi_buffer_read_bytes(sbdi_buffer_t *buf, uint8_t *dest,
     const size_t length);
 
 /*!
- * \brief Writes length bytes from src into the buffer
- * @param buf[inout] a pointer to the buffer to write to
- * @param src[in] a pointer to the data buffer to read from
- * @param length[in] the number of bytes to copy from src into the buffer
+ * \brief Reads a 128b counter from the buffer
+ *
+ * @param buf[inout] a pointer to the buffer to read from
+ * @param ctr[out] a pointer to the 128 bit counter to read from the buffer
+ * @returns SBDI_SUCCESS if the counter could be initialized;
+ *          SBDI_ILLEGAL_PARAM otherwise
  */
-void sbdi_buffer_write_bytes(sbdi_buffer_t *buf, const uint8_t *src,
-    const size_t length);
+sbdi_error_t sbdi_buffer_read_ctr_128b(sbdi_buffer_t *buf, sbdi_ctr_128b_t *ctr);
 
 #endif /* SBDI_BUFFER_H_ */
 
