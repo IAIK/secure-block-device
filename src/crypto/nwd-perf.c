6e56605b5fc4db06884e0e0f50584c55986ffc43
Johannes Winter <johannes.winter@iaik.tugraz.at>
2015-03-19 13:36:17 +0100

    sbdi: start work on normal world performance test

---
+++ src/crypto/nwd-perf.c
@@ -0,0 +1,16 @@
+///
+/// \file
+/// \brief Normal World Crypto Performance Test
+///
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "sbdi_nocrypto.h"
+#include "sbdi_ocb.h"
+#include "sbdi_siv.h"
+
+//----------------------------------------------------------------------
+int main(void)
+{
+  return 0;
+}
