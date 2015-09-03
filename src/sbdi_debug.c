16074be15022e153ac8665c916bf3e706122cb3e
Daniel Hein <daniel.hein@iaik.tugraz.at>
2015-09-03 16:21:02 +0200

    add license to source files

--- src/sbdi_debug.c
+++ src/sbdi_debug.c
@@ -1,3 +1,27 @@
+/* Copyright (c) IAIK, Graz University of Technology, 2015.
+ * All rights reserved.
+ * Contact: http://opensource.iaik.tugraz.at
+ * 
+ * This file is part of the Secure Block Device Library.
+ * 
+ * Commercial License Usage
+ * Licensees holding valid commercial licenses may use this file in
+ * accordance with the commercial license agreement provided with the
+ * Software or, alternatively, in accordance with the terms contained in
+ * a written agreement between you and SIC. For further information
+ * contact us at http://opensource.iaik.tugraz.at.
+ * 
+ * Alternatively, this file may be used under the terms of the GNU General
+ * Public License as published by the Free Software Foundation version 2.
+ * 
+ * The Secure Block Device Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with the Secure Block Device Library. If not, see <http://www.gnu.org/licenses/>.
+ */
 ///
 /// \file
 /// \brief Implements functions used to debug the Secure Block Device Library.
